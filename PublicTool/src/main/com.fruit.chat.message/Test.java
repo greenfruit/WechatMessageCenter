3073d93299cc24248f93abd7d3dd2009de8db6e4
zhangshouzheng <zhangshouzheng@le.com>
2017-01-19 15:43:52 +0800

    init project message center.

---
+++ PublicTool/src/main/com.fruit.chat.message/Test.java
@@ -0,0 +1,15 @@
+/**
+ * <p>DESCRIPTION:  类描述
+ * <p>CALLED BY:   zhangshouzheng
+ * <p>UPDATE BY:   zhangshouzheng
+ * <p>CREATE DATE: 2017/1/19
+ * <p>UPDATE DATE: 2017/1/19
+ *
+ * @version 1.0
+ * @since java 1.7.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello,world.");
+    }
+}
